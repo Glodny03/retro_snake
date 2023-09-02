adb55956c078e81eec48a00c5b73ebe5b2f158db
Glodny03 <jakubmrzyglod03@gmail.com>
2023-09-02 20:00:04 +0200

    Load game assets

--- scripts/snake.js
+++ scripts/snake.js
@@ -1,9 +1,14 @@
 (function () {
-   
+    // Load assets
+    const foodSound = new Audio('../assets/sounds/beep.mp3');
+    const gameOverSound = new Audio('../assets/sounds/over.mp3');
+
+    const foodSvg = new Image();
+    foodSvg.src = '../assets/food.svg';
     // Initialize the game canvas, event listeners, and main loop
     function startGame() {
-     
-        }
+
+    }
 
     document.querySelector(".play-button").addEventListener("click", startGame);
     window.onload = startGame; // Start the game when the window loads
