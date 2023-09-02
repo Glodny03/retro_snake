5e9510cdad73976a3777be3d1bc556aa9db1226b
Glodny03 <jakubmrzyglod03@gmail.com>
2023-09-02 20:00:45 +0200

    Initialize game variables and constants

--- scripts/snake.js
+++ scripts/snake.js
@@ -5,6 +5,30 @@
 
     const foodSvg = new Image();
     foodSvg.src = '../assets/food.svg';
+
+    // Initialize global variables
+    const snakeElementSize = 25;
+    const initialSnakeLength = 5;
+    const gameLoopIntervalMs = 120;
+
+    // Game state variables
+    let isSoundEnabled = true;
+    let canvas;
+    let context2d;
+    let snake = [];
+    let snakeVelocityX = 0;
+    let snakeVelocityY = 0;
+    let pauseGame = true;
+    let food = {
+        x: 0,
+        y: 0,
+        color: "white"
+    };
+    let points = 0;
+    let bestScore = 0;
+    let currentRound = 0;
+    let foodRotationAngle = 0;
+
     // Initialize the game canvas, event listeners, and main loop
     function startGame() {
 
