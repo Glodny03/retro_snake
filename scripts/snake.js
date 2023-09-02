9762a437a79f2cf2ee62e6d159cd8ddbb0f5aeff
Glodny03 <jakubmrzyglod03@gmail.com>
2023-09-02 19:59:16 +0200

    Create basic JavaScript structure

---
+++ scripts/snake.js
@@ -0,0 +1,10 @@
+(function () {
+   
+    // Initialize the game canvas, event listeners, and main loop
+    function startGame() {
+     
+        }
+
+    document.querySelector(".play-button").addEventListener("click", startGame);
+    window.onload = startGame; // Start the game when the window loads
+})();
